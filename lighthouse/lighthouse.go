0f62601b9818dd4a8f5a8837090b98c2ea2531aa
ninedraft <clamores.pro@gmail.com>
2020-08-26 21:32:26 +0300

    add lighthouse tools

---
+++ lighthouse/lighthouse.go
@@ -0,0 +1,86 @@
+// Package lighthouse provides lighthouse runners.
+package lighthouse
+
+import (
+	"bufio"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/url"
+	"os/exec"
+	"strings"
+)
+
+// Config describes lighthouse runner settings.
+type Config struct {
+	Exec string
+	Args []string
+}
+
+// Run lighthouse task.
+func (cfg Config) Run(page string, report io.Writer) error {
+	var _, errURL = url.Parse(page)
+	if errURL != nil {
+		return fmt.Errorf("page URL: %w", errURL)
+	}
+	var stderr = &bytes.Buffer{}
+	var stdout = &bytes.Buffer{}
+	var cmd = &exec.Cmd{
+		Path: cfg.Exec,
+		Args: cfg.args(
+			"--output", "json",
+			page,
+		),
+		Stdout: stdout,
+		Stderr: stderr,
+	}
+	if err := cmd.Run(); err != nil {
+		return fmt.Errorf("running lighthouse tool: %w: ", err)
+	}
+	var _, errCopy = stdout.WriteTo(report)
+	if errCopy != nil {
+		var msg = parseErrorMessage(stderr.Bytes())
+		return fmt.Errorf("writing report: %w: %s", errCopy, msg)
+	}
+	if err := validateJSON(stdout.Bytes()); err != nil {
+		return fmt.Errorf("report: %w", err)
+	}
+	return nil
+}
+
+func (cfg Config) args(extra ...string) []string {
+	var n = len(cfg.Args)
+	return append(cfg.Args[:n:n], extra...)
+}
+
+func parseErrorMessage(data []byte) string {
+	var sep = []byte(" ")
+	var msg = &strings.Builder{}
+	var re = bytes.NewReader(data)
+	var sc = bufio.NewScanner(re)
+	for sc.Scan() {
+		var line = sc.Bytes()
+		if bytes.HasPrefix(line, sep) {
+			continue
+		}
+		_, _ = msg.Write(line)
+		_, _ = msg.WriteString(" ")
+	}
+	if msg.Len() == 0 {
+		return string(data)
+	}
+	return msg.String()
+}
+
+func validateJSON(data []byte) error {
+	var re = bytes.NewReader(data)
+	var decoder = json.NewDecoder(re)
+	for decoder.More() {
+		var _, err = decoder.Token()
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
